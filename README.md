d645c2996553ae9d2251ceef5516840934fb1ad6
lituokobe <lituolituolituo@gmail.com>
2025-06-05 18:21:02 +0800

    added flowchart and modified README.md

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-## Ctrip AI Assistant ##
-Ctrip is an advanced online travel agency in China, providing flight booking, hotel booking, travel plans for millions of customers.  
+# Ctrip AI Assistant  
 
-This project is to use LLM with LangChain to simulate a Ctrip AI assistant with dummy data.
+<p align="center">
+  <img src="./Ctrip-Logo.png" alt="Ctrip Logo" width="170">
+</p>
+
+Ctrip is a leading online travel agency in China, offering flight bookings, hotel reservations, and customized travel plans to millions of customers.  
+
+This project leverages **LLMs** with **LangChain** to simulate a Ctrip AI assistant using dummy data. **Any LLM model can be used for this project**, and it can be modified in `./graph_chat/llm_tavily.py`.  
+
+## Features  
+
+The AI assistant interacts with users who have already booked a flight through Ctrip, enabling them to:  
+- Check flight status  
+- Modify flights  
+- Book additional travel services, including hotels, car rentals, and excursions  
+
+## Architecture  
+
+The assistant consists of a **primary_assistant** responsible for engaging with the user and answering general inquiries, such as flight status, policy searches, and other web-based queries.  
+
+For specific travel-related services, the **primary_assistant** delegates tasks to specialized assistants:  
+- **update_flight** – Handles flight modifications  
+- **book_hotel** – Manages hotel reservations  
+- **book_car_rental** – Facilitates car rentals  
+- **book_excursion** – Arranges excursions  
+
+All **sensitive operations** involving database changes, such as flight modifications or new bookings, require **explicit user approval** before execution.  
+
+## Workflow  
+
+This project is built using **LangChain**. Below is a flowchart illustrating the assistant's operational mechanics:  
+
+![Ctrip AI Assistant Flowchart](./Ctrip_AI_assistant_flowchart.png)  
+
+## Setup Instructions  
+
+Before running the project, follow these steps to set up your environment:  
+
+1. **Download the project repository** to your local machine.  
+2. **Download the database files** from [this public link](https://drive.google.com/drive/folders/1o6Qu7xNAqr1PrrfzmZzcdBpoErJpr788?usp=sharing) and copy them to the project folder.  
+3. **Set up your LLM model API key** by modifying `./graph_chat/llm_tavily.py`.  
+
+## Running the Project  
+
+To experience the assistant in a **prompt-based interface**, run:  
+```bash
+python ./graph_chat/graph3.py
+```
+To interact with the AI assistant in a web GUI powered by Gradio, run:
+```bash
+python ./graph_chat/graph_gradio.py
+```
---
+++ graph_chat/graph_gradio.py
@@ -0,0 +1,185 @@
+from typing import List, Dict
+
+import gradio as gr
+import uuid
+
+from langchain_core.messages import ToolMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph
+from langgraph.prebuilt import tools_condition
+
+from Ctrip_AI_assistant.graph_chat.assistant import assistant_runnable, CtripAssistant, primary_assistant_tools
+from Ctrip_AI_assistant.graph_chat.base_data_model import ToFlightBookingAssistant, ToBookCarRental, \
+    ToHotelBookingAssistant, ToBookExcursion
+from Ctrip_AI_assistant.graph_chat.build_child_graph import build_flight_graph, builder_hotel_graph, build_car_graph, \
+    builder_excursion_graph
+from Ctrip_AI_assistant.graph_chat.state import State
+from Ctrip_AI_assistant.tools.init_db import update_dates
+from Ctrip_AI_assistant.tools.tools_handler import create_tool_node_with_fallback
+from Ctrip_AI_assistant.tools.flights_tools import fetch_user_flight_information
+
+#define a construction object for the graph
+builder = StateGraph(State)
+
+def get_user_info(state: State):
+    """
+    get passenger's flight info and update state dictionary
+    :param state: current state dictionary
+    :return: new state dictionary including user inf
+    """
+    return {"user_info": fetch_user_flight_information.invoke({})}
+
+#fetch_user_info is executed first, meaning we can get user's flight information before doing anything
+builder.add_node('fetch_user_info', get_user_info)
+#add edges
+builder.add_edge(START, 'fetch_user_info')
+
+#add child graph of 4 special assistant
+builder = build_flight_graph(builder)
+builder = builder_hotel_graph(builder)
+builder = build_car_graph(builder)
+builder = builder_excursion_graph(builder)
+
+#add main assistant
+builder.add_node('primary_assistant', CtripAssistant(assistant_runnable))
+builder.add_node('primary_assistant_tools', create_tool_node_with_fallback(primary_assistant_tools))
+
+def route_primary_assistant(state: dict):
+    """
+    Based on current state, decide which child assistant node to route to.
+    :param state: dictionary of current dialog state
+    :return: node name of next step
+    """
+    route = tools_condition(state)  # decide next step
+    if route == END:
+        return END  # if the ending condition is satisfied, return END
+    tool_calls = state["messages"][-1].tool_calls  # get the tool call in the last message
+    if tool_calls:
+        if tool_calls[0]["name"] == ToFlightBookingAssistant.__name__:
+            return "enter_update_flight"  #go to flight update node
+        elif tool_calls[0]["name"] == ToBookCarRental.__name__:
+            return "enter_book_car_rental"  # go to car rental booking node
+        elif tool_calls[0]["name"] == ToHotelBookingAssistant.__name__:
+            return "enter_book_hotel"  # go to hotel booking node
+        elif tool_calls[0]["name"] == ToBookExcursion.__name__:
+            return "enter_book_excursion"  # go to excursion booking node
+        return "primary_assistant_tools"  # otherwise go to primary assistant node
+    raise ValueError("Invalid route")  # If cannot find appropriate tool calls, raise an error
+
+
+#add conditional edges
+builder.add_conditional_edges(
+    "primary_assistant",
+    route_primary_assistant,
+    [
+        "enter_update_flight",
+        "enter_book_car_rental",
+        "enter_book_hotel",
+        "enter_book_excursion",
+        "primary_assistant_tools",
+        END,
+    ]
+)
+
+builder.add_edge('primary_assistant_tools', 'primary_assistant')
+
+# Every child graph with assigned task can respond to user directly. When user replies, we hope to return to current graph
+def route_to_workflow(state: dict) -> str:
+    """
+    If we are in a state of being assigned, directly route to respective assistant.
+    :param state: dictionary of current dialog state
+    :return: the node name to go
+    """
+    dialog_state = state.get("dialog_state")
+    if not dialog_state:
+        return "primary_assistant"  # of no dialog state, return to main assistant
+    return dialog_state[-1]  # otherwise return to the last assistant
+
+
+builder.add_conditional_edges("fetch_user_info", route_to_workflow)  # route based on user info
+
+memory = MemorySaver()
+graph = builder.compile(
+    checkpointer=memory,
+    interrupt_before=[
+        "update_flight_sensitive_tools",
+        "book_car_rental_sensitive_tools",
+        "book_hotel_sensitive_tools",
+        "book_excursion_sensitive_tools",
+    ]
+)
+
+session_id = str(uuid.uuid4())
+update_dates() #make sure the database is updated before every testing
+
+config = {
+    "configurable": {
+        #passenger_id is for flight tool to get flight info
+        "passenger_id": "3442 587242",
+        #checkpointer is visited by thread_id
+        "thread_id": session_id,
+    }
+}
+
+def do_graph(user_input, chat_bot):
+    """
+    function to execute after input is submitted
+    :return:
+    """
+    if user_input:
+        chat_bot.append({'role':'user', 'content': user_input})
+
+    return '', chat_bot
+
+def execute_graph(chat_bot: List[Dict]) -> List[Dict]:
+    """
+    function to execute the workflow
+    """
+    user_input = chat_bot[-1]['content']
+    result = '' #AI assistant last message
+
+    if user_input.strip().lower() !='y': #regular user question
+        events = graph.stream({"messages": ("user", user_input)}, config, stream_mode = "values")
+    else: # user inputs 'y':
+        events = graph.stream(None, config, stream_mode="values")
+
+    for event in events:
+        messages = event.get("messages")
+        if messages:
+            if isinstance(messages, list):
+                message = messages[-1]
+            if message.__class__.__name__ == "AIMessage":
+                if message.content:
+                    result = message.content #messages that needs to display in web UI
+            msg_repr = message.pretty_repr(html = True)
+            if len(msg_repr) > 1500:
+                msg_repr = msg_repr[:1500] + "... and more."
+            print(msg_repr)
+
+    current_state = graph.get_state(config)
+    if current_state.next: #interruption happens
+        result = "Do you approve above operation? input 'y' to continue, otherwise specify your requests.\n"
+
+    chat_bot.append({'role':'assistant', 'content': result})
+
+    return chat_bot
+
+
+#buils a UI with gradio
+css = """
+#bgc {background-color: #8FEFA3}
+.feedback textarea {font-size: 24px !important}
+"""
+with gr.Blocks(title='Ctrip AI Assistant', css=css) as instance: #set up the page title with css, we have an HTML page
+    gr.Label('Ctrip AI Assistant', container=False) #header of the page
+
+    chatbot = gr.Chatbot(type='messages', height=350, label = 'AI Assistant') #chatbot widget
+
+    input_textbox = gr.Textbox(label='Please input your question.📝', value='') #input box
+
+    input_textbox.submit(do_graph, [input_textbox, chatbot], [input_textbox, chatbot]).then(execute_graph, chatbot, chatbot)
+
+if __name__=='__main__':
+    #launch the gradio app
+    instance.launch(debug=True)
